a8bb780401d54e3b683eaedf349c6be71768cfd5
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-09-20 13:10:08 +0200

    update schemas

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  id: Joi.string().guid({ version: ['uuidv4'] }).required(),
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
