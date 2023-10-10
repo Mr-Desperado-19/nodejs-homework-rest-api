70be579bfbbd5c7ecead6a1a6ac2132a295989de
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-10-10 17:50:20 +0200

    Added contact owner protection

--- models/contacts.js
+++ models/contacts.js
@@ -22,9 +22,42 @@ const contactSchema = new mongoose.Schema({
   }
 }, { versionKey: false });
 
+contactSchema.statics.listContactsByOwner = async function (userId) {
+  return this.find({ owner: userId });
+};
+
+contactSchema.statics.getContactByIdByOwner = async function (contactId, userId) {
+  return this.findOne({ _id: contactId, owner: userId });
+};
+
+contactSchema.statics.addContactByOwner = async function (contact, userId) {
+  return this.create({ ...contact, owner: userId });
+};
+
+contactSchema.statics.removeContactByOwner = async function (contactId, userId) {
+  return this.findOneAndRemove({ _id: contactId, owner: userId });
+};
+
+contactSchema.statics.updateContactByOwner = async function (contactId, update, userId) {
+  return this.findOneAndUpdate({ _id: contactId, owner: userId }, update, {
+    new: true,
+  });
+};
+
+contactSchema.statics.updateFavoriteContactByOwner = async function (contactId, favorite, userId) {
+  return this.findOneAndUpdate(
+    { _id: contactId, owner: userId },
+    { favorite },
+    {
+      new: true,
+    }
+  );
+};
+
 const Contact = mongoose.model("Contact", contactSchema);
 
 module.exports = Contact;
+
 
 
 
